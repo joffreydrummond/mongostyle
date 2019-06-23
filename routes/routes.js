d73123933ff3eb81eb9298f76efd291d496c9789
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2019-06-22 18:35:42 -0700

    made changes to server.js

---
+++ routes/routes.js
@@ -0,0 +1,81 @@
+const express = require("express"),
+logger = require("morgan"),
+// mongoose = require("mongoose"),
+axios = require("axios"),
+  cheerio = require("cheerio"),
+  db = require("./models"),
+  app = express();
+
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("views"));
+
+
+app.get("/scrape", function (req, res) {
+  axios.get("https://old.reddit.com/r/Vaping/").then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    $("p.title").each(function(i, element) {
+      var result = {};
+
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+
+    res.send("Scraped It So Good");
+  });
+});
+
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+   
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, function() {
+  console.log("App is live on port " + PORT + "!");
+});
